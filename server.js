f439fa776cf2962bcc2f8a0a74226fdf964037ed
Jorie Fernandez <joriefefernandez@yahoo.com>
2019-05-13 15:39:59 -0700

    Added URL object and hash method

--- server.js
+++ server.js
@@ -4,12 +4,31 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const sha256 = require('js-sha256');
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
+
+//Database
+const client = new pg.Client(process.env.DATABASE_URL);
+client.connect();
 
 app.get('/hello', (request, response) => {
   response.status(200).send('Hello');
 });
 
 app.listen(PORT,() => console.log(`Listening on port ${PORT}`));
+
+//URL OBJECT
+
+function URL (long_url) {
+  this.long_url = long_url,
+  this.short_url = '',
+  this.clicks = 0;
+}
+
+// Method for creating short_url hash
+URL.prototype.create_hash = function() {
+  this.short_url = sha256(this.long_url).slice(0,4);
+  console.log('Short URL', this.short_url);
+};
