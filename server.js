2118b6f85884b5665e07ca221fabb1e7ace9ca12
Charles Clemens <cclemensjr@gmail.com>
2019-05-14 09:38:37 -0700

    Fixed dependency error

--- server.js
+++ server.js
@@ -3,13 +3,17 @@
 /*********
  * MIDDLEWARE SETUP
  */
+
 require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const superagent = require('superagent');
+const pg = require('pg');
 const sha256 = require('js-sha256');
 const PORT = process.env.PORT || 3000;
 
+//static files
 app.use(cors());
 
 //Database
